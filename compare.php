5c2387686dc07d540282d4ac3815ab73e8e3d4e2
cj-dale <C24Caleb.Dale@afacademy.af.edu>
2023-05-10 20:35:30 -0700

    two "nice" tables

--- compare.php
+++ compare.php
@@ -28,12 +28,13 @@ session_start();
         const username = "<?php echo $_SESSION['username'] ?>";
         document.write("Logged in as: " + username);
     </script>
-    <?php if (isset($_SESSION["username"])): ?>
+    <?php $customization_set = 0;
+    if (isset($_SESSION["username"])): ?>
         <form action="logout.php" method="post">
             <input type="submit" value="Log out">
         </form>
+    
     <?php endif; ?>
-
 
     <?php
     if (isset($_SESSION['username'])) {
@@ -44,7 +45,7 @@ session_start();
         $table->execute();
         $results = $table->get_result();
 
-        if ($results->num_rows > 0) {
+        if ($results->num_rows > 0 && $customization_set == 0) {
             echo ('<h3>Customization 1:</h3>
         <form method="post" action="compare.php"> 
             <select name="customization1" id="customization1">');
@@ -90,27 +91,73 @@ session_start();
                 INNER JOIN vehicles ON customizations.vehicle = vehicles.name
                 INNER JOIN wheels ON customizations.wheel = wheels.name
                 INNER JOIN gliders ON customizations.glider = gliders.name
-                WHERE customizations.customization_name = ? OR customizations.customization_name = ?
+                WHERE customizations.customization_name = ?
 ");
+
                 if (!$statement1) {
                     die("Error in query: " . $connection->error);
                 }
-                $statement1->bind_param("ss", $customization1, $customization2);
+                $statement1->bind_param("s", $customization1);
                 $statement1->execute();
                 if ($statement1->error) {
                     die("Error in query: " . $statement1->error);
                 }
-                $result = $statement1->get_result();
-                if ($result) {
-                    while ($row = $result->fetch_assoc()) {
-                        echo "Total Speed: " . $row["total_speed"] . "<br>";
-                        echo "Total Acceleration: " . $row["total_acceleration"] . "<br>";
-                        echo "Total Traction: " . $row["total_traction"] . "<br>";
-                        echo "Total Handling: " . $row["total_handling"] . "<br>";
-                        echo "Total Mini Turbo: " . $row["total_mini_turbo"] . "<br><br>";
+                $result1 = $statement1->get_result();
+                if ($result1) {
+                    echo "<br><br> <table>";
+                    echo "<tr><th>Customization</th><th>Total Speed</th><th>Total Acceleration</th><th>Total Traction</th><th>Total Handling</th><th>Total Mini Turbo</th></tr>";
+                    while ($row = $result1->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . "1" . "</td>";
+                        echo "<td>" . $row["total_speed"] . "</td>";
+                        echo "<td>" . $row["total_acceleration"] . "</td>";
+                        echo "<td>" . $row["total_traction"] . "</td>";
+                        echo "<td>" . $row["total_handling"] . "</td>";
+                        echo "<td>" . $row["total_mini_turbo"] . "</td>";
+                        echo "</tr>";
                     }
+                    echo "</table>";
                 }
 
+                $statement2 = $connection->prepare("
+                SELECT 
+                    SUM(characters.speed + vehicles.speed + wheels.speed + gliders.speed) AS total_speed,
+                    SUM(characters.acceleration + vehicles.acceleration + wheels.acceleration + gliders.acceleration) AS total_acceleration,
+                    SUM(characters.traction + vehicles.traction + wheels.traction + gliders.traction) AS total_traction,
+                    SUM(characters.handling + vehicles.handling + wheels.handling + gliders.handling) AS total_handling,
+                    SUM(characters.miniturbo + vehicles.miniturbo + wheels.miniturbo + gliders.miniturbo) AS total_mini_turbo
+                FROM customizations
+                INNER JOIN characters ON customizations.character_name = characters.name
+                INNER JOIN vehicles ON customizations.vehicle = vehicles.name
+                INNER JOIN wheels ON customizations.wheel = wheels.name
+                INNER JOIN gliders ON customizations.glider = gliders.name
+                WHERE customizations.customization_name = ?
+");
+
+                if (!$statement2) {
+                    die("Error in query: " . $connection->error);
+                }
+                $statement2->bind_param("s", $customization2);
+                $statement2->execute();
+                if ($statement2->error) {
+                    die("Error in query: " . $statement2->error);
+                }
+                $result2 = $statement2->get_result();
+                if ($result2) {
+                    echo "<table>";
+                    echo "<tr><th>Customization</th><th>Total Speed</th><th>Total Acceleration</th><th>Total Traction</th><th>Total Handling</th><th>Total Mini Turbo</th></tr>";
+                    while ($row = $result2->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . "2" . "</td>";
+                        echo "<td>" . $row["total_speed"] . "</td>";
+                        echo "<td>" . $row["total_acceleration"] . "</td>";
+                        echo "<td>" . $row["total_traction"] . "</td>";
+                        echo "<td>" . $row["total_handling"] . "</td>";
+                        echo "<td>" . $row["total_mini_turbo"] . "</td>";
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                }
 
                 ?>
                 </div>
